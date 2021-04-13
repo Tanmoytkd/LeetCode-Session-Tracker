b3851ef560d450249cfec3b19f0d9820d4bd95b4
Tanmoy Krishna Das <tanmoykrishnadas@gmail.com>
2021-04-13 20:46:30 +0600

    Add system to change session from extension

--- ROADMAP.md
+++ ROADMAP.md
@@ -1,9 +1,9 @@
 # LeetCode Session Tracker Extension Roadmaps
 
-0. Add system to change session from extension
-1. Create Usable UI
-2. Convert UI to React
-3. Show how much time left until next Reload
-4. Show Current Progress Statistics
-5. Leetcode Monthly Challenge Integration
-6. Add other online Judges
+- [x] Add system to change session from extension
+- [ ] Create Usable UI
+- [ ] Convert UI to React
+- [ ] Show how much time left until next Reload
+- [ ] Show Current Progress Statistics
+- [ ] Leetcode Monthly Challenge Integration
+- [ ] Add other online Judges
--- src/js/popup.js
+++ src/js/popup.js
@@ -6,21 +6,41 @@
 let reloadSessionDataBtn = document.getElementById("reloadSessionData");
 let sessionDataDiv = document.getElementById("sessionData");
 
-function showSessionData() {
-  chrome.storage.sync.get("leetCodeProgress", ({ leetCodeProgress }) => {
-    let sessionData = undefined;
+function showSessionList(leetCodeProgress) {
+  let sessionList = leetCodeProgress.sessionList;
+  if (sessionList.length === 0) {
+    $("#sessionChangeForm").hide();
+  } else {
+    $("#newSession").html("");
 
-    if (leetCodeProgress.loading) {
-      sessionData = "Loading...";
-    } else {
-      if (leetCodeProgress.sessionName == undefined) {
-        sessionData = "You are Logged out right now";
-      } else {
-        sessionData = leetCodeProgress.sessionName;
-      }
+    $("<option>")
+      .val("")
+      .text(leetCodeProgress.sessionName)
+      .appendTo($("#newSession"));
+
+    for (const session of sessionList) {
+      $("<option>")
+        .val(session.id)
+        .text(session.name)
+        .appendTo($("#newSession"));
     }
 
-    sessionDataDiv.innerText = sessionData;
+    $("#sessionChangeForm").show();
+  }
+}
+
+function showSessionData() {
+  chrome.storage.sync.get("leetCodeProgress", ({ leetCodeProgress }) => {
+    if (leetCodeProgress.loading) {
+      sessionDataDiv.innerText = "Loading...";
+    } else if (leetCodeProgress.sessionName == undefined) {
+      sessionDataDiv.innerText = "You are Logged out right now";
+    } else {
+      sessionData = leetCodeProgress.sessionName;
+      sessionDataDiv.innerText = sessionData;
+
+      showSessionList(leetCodeProgress);
+    }
   });
 }
 
@@ -37,4 +57,46 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
 reloadSessionDataBtn.addEventListener("click", async () => {
   loadProgressData();
   console.log(`Session Reloaded: ${leetCodeProgress.sessionName}`);
+});
+
+$("#newSession").change(async () => {
+  $("#newSession option:selected").each(async function () {
+    if ($(this).val() != undefined && $(this).val() != "") {
+      let sessionId = parseInt($(this).val());
+      console.log(sessionId);
+
+      try {
+        let response = await fetch("https://leetcode.com/session/", {
+          headers: {
+            accept: "application/json, text/javascript, */*; q=0.01",
+            "accept-language": "en-US,en;q=0.9,bn;q=0.8",
+            "cache-control": "no-cache",
+            "content-type": "application/json",
+            pragma: "no-cache",
+            "sec-ch-ua":
+              '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-requested-with": "XMLHttpRequest",
+          },
+          referrer: "https://leetcode.com/problemset/all/",
+          referrerPolicy: "strict-origin-when-cross-origin",
+          body: `{"func":"activate","target":${sessionId}}`,
+          method: "PUT",
+          mode: "cors",
+          credentials: "include",
+        });
+
+        let data = await response.json();
+        loadProgressData();
+
+        console.log("Change Session:");
+        console.log(data);
+      } catch (error) {
+        console.error(`Error: ${error}`);
+      }
+    }
+  });
 });
